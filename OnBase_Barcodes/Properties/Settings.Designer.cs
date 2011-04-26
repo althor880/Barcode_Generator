b7dd159fd58f3022a7d9d98b121241608622bf94
althor880 <althor880@gmail.com>
2011-04-26 15:50:36 -0400

    Fixed saving of settings (i.e. connections).

--- OnBase_Barcodes/Properties/Settings.Designer.cs
+++ OnBase_Barcodes/Properties/Settings.Designer.cs
@@ -23,13 +23,16 @@ namespace OnBase_Barcodes.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
         public global::System.Collections.Specialized.StringCollection ConnectionStrings {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["ConnectionStrings"]));
+            }
+            set {
+                this["ConnectionStrings"] = value;
             }
         }
         
